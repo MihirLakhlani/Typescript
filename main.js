bbc4842f1209960fbc24457d95643c629d0316ec
mihir lakhlani <mihirlakhlani2000@gmail.com>
2021-10-25 13:59:06 +0530

    async

---
+++ main.js
@@ -0,0 +1,16 @@
+console.log("mihir lakhlani");
+
+
+
+setTimeout(() => {
+    for (let i =1;i<=49   ;i++){
+        let element = i;
+        console.log("this is number "+i)
+    
+    }
+    
+},4000);
+
+
+
+console.log("done printing")
